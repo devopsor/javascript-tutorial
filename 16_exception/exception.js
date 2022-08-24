040a64bff9d3b87e42398a35487a5db85532effc
devopsor <saekiemail@gmail.com>
2022-08-25 01:04:49 +0900

    update exception tutorials

--- 16_exception/exception.js
+++ 16_exception/exception.js
@@ -1,4 +1,37 @@
-// print 'Hello World' in JavaScript.
-var result = "Hello World!";
-console.log(result);
+function main(s) {
+    console.log('BEGIN main()');
+    try {
+        foo(s);
+    } catch (e) {
+        console.log('Exception：' + e);
+    }
+    console.log('END main()');
+}
+
+function foo(s) {
+    console.log('BEGIN foo()');
+    bar(s);
+    console.log('END foo()');
+}
+
+function bar(s) {
+    console.log('BEGIN bar()');
+    console.log('length = ' + s.length);
+    console.log('END bar()');
+}
+
+main(null);
+
+function printTime() {
+    try{
+        throw new Error();
+    }catch (e) {
+        console.log('printError');
+    }
+}
+
+setTimeout(printTime, 1000);
+console.log('done');
+
+
 
